2592fb4dfa196e78275e8233dc77514e9591b1ab
Artur Guedes <arturguedx@gmail.com>
2019-04-22 18:26:54 -0300

    c4

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var CACHE_VERSION = '1.6'
 var CACHE_NAME = 'cache-v' + CACHE_VERSION;
 
-const PRECACHE = 'precache-v3';
+const PRECACHE = 'precache-v4';
 const RUNTIME = 'runtime';
 
 var CACHE_FILES = [
