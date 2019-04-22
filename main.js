93d41ba2fd10aab13d0ca4c551dcb33151830b76
Artur Guedes <arturguedx@gmail.com>
2019-04-22 17:57:43 -0300

    Teste 2

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+
+(function () {
+    var old = console.log;
+    var logger = document.querySelector('.log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += '<code> <i>Log:</i> ' + message + '</code><br />';
+        }
+    }
+})();
--- sw.js
+++ sw.js
@@ -1,32 +1,32 @@
-var CACHE_NAME = 'cache-v1.1';
+var CACHE_VERSION = '1.2'
+var CACHE_NAME = 'cache-v' + CACHE_VERSION;
 var urlsToCache = [
   '/',
   '/main.css',
   '/main.js'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Abriu do cache');
-        return cache.addAll(urlsToCache);
-      })
+    .then(function (cache) {
+      console.log('Abriu do cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
