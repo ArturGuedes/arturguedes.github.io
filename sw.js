7fcbe0838b9b4393eb9c625acdb5a28494844792
Artur Guedes <arturguedx@gmail.com>
2019-04-22 18:12:47 -0300

    Cache 1.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = '1.4'
+var CACHE_VERSION = '1.5'
 var CACHE_NAME = 'cache-v' + CACHE_VERSION;
 var urlsToCache = [
   '/',
@@ -48,4 +48,21 @@ self.addEventListener('fetch', function(event) {
         );
       })
     );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
