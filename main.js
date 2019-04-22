b96e7a780df0094f2138d323e22c9a2357412d7c
Artur Guedes <arturguedx@gmail.com>
2019-04-22 18:08:06 -0300

    Teste 3

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('sw.js').then(function (registration) {
             // Registro OK
-            console.log('ServiceWorker registrado com sucesso com scope: ', registration.scope);
+            console.log('ServiceWorker registrado com sucesso: ', registration.scope);
         }, function (err) {
             // Ragistro não funcionou :(
             console.log('Falha no registro do ServiceWorker: ', err);
