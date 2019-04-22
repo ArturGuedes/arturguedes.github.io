d0c364fca634c243b71a965f5c1290b4f3191ad9
Artur Guedes <arturguedx@gmail.com>
2019-04-22 19:09:53 -0300

    Cache 1.9.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = '1.9.3'
+var CACHE_VERSION = '1.9.4'
 var CACHE_NAME = 'cache-v' + CACHE_VERSION;
 var urlsToCache = [
   '/',
@@ -19,13 +19,10 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(async function() {
-    const cache = await caches.open(CACHE_NAME);
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) return cachedResponse;
-    const networkResponse = await fetch(event.request);
-    event.waitUntil(
-      cache.put(event.request, networkResponse.clone())
-    );
-    return networkResponse;
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
   }());
 });
