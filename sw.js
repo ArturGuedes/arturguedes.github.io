eefa468a9aee2193dd363ce7ad35a910596980ad
Artur Guedes <arturguedx@gmail.com>
2019-04-22 18:06:43 -0300

    Cache 1.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = '1.2'
+var CACHE_VERSION = '1.4'
 var CACHE_NAME = 'cache-v' + CACHE_VERSION;
 var urlsToCache = [
   '/',
@@ -18,15 +18,34 @@ self.addEventListener('install', function (event) {
 });
 
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-    .then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
